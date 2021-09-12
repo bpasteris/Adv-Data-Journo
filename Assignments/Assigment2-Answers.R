51b160e8e7175eae1e023496d5caf9865da3327e
bpasteris <89530674+bpasteris@users.noreply.github.com>
2021-09-12 11:41:55 -0500

    Turn in Assignment 2

---
+++ Assignments/Assigment2-Answers.R
@@ -0,0 +1,63 @@
+#### ASSIGNMENT 2 ####
+
+# Answer the following questions using the dispatch.csv file 
+# and the tidyverse functions we've learned 
+
+
+# Question 1: What's the timeframe that this dataset covers?  
+
+# Your code: dispatch %>% select(CallDateTime) %>% arrange(CallDateTime) and dispatch %>% select(CallDateTime) %>% arrange(desc(CallDateTime))
+
+
+
+
+# The answer: 6 days, 8/23 to 8/29
+
+
+
+
+# Question 2: Which day of the week had the most incidents? 
+
+# Your code: dispatch %>% group_by(DOW) %>% summarise(num = n()) %>% arrange(desc(num))
+
+
+
+
+# The answer: Tuesday
+
+
+
+
+# Question 3:  Which hour of the day had the most incidents?
+
+# Your code: dispatch %>% group_by(Hour) %>% summarise(num = n()) %>% arrange(desc(num))
+
+
+
+
+# The answer: 13
+
+
+
+
+# Question 4:  Which police district had the most traffic stops?
+
+# Your code: dispatch %>% filter(ExtNatureDisplayName == "TRAFFIC") %>% group_by(PolArea) %>% summarise(num = n()) %>% arrange(desc(num))
+
+
+
+
+# The answer: 10W
+
+
+
+
+
+# Question 5:  What's the most common reason police are dispatched to the airport? (11300 S AIRPORT DR)
+
+# Your code: dispatch %>% filter(Address == "11300 S AIRPORT DR") %>% group_by(ExtNatureDisplayName) %>% summarise(num = n()) %>% arrange(desc(num))
+
+
+
+
+# The answer: CHECK AREA
